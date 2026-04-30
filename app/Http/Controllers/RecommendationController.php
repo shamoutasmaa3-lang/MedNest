20f2c08c4464b3d699e06f4479a07fd9edfcbe05
Areej <ablaarooj3@gmail.com>
2026-04-30 20:38:22 +0300

    Add Recommendation Controller

--- app/Http/Controllers/RecommendationController.php
+++ app/Http/Controllers/RecommendationController.php
@@ -7,13 +7,14 @@ use Illuminate\Http\Request;
 
 class RecommendationController extends Controller
 {
+
     public function index(Request $request)
     {
         return Recommendation::where('user_id', $request->user()->id)
             ->orderBy('created_at', 'desc')
             ->get();
     }
-
+//
     public function markAsRead($id)
     {
         $rec = Recommendation::findOrFail($id);
